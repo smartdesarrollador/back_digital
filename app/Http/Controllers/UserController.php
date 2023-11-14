e7b8ac3e27513f5ab891f6a2f7a054f1b763fd21
Jeans <sistemadesignstyle@gmail.com>
2023-11-13 23:55:05 -0500

    condicion correo existe

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,14 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function register(Request $request){
-        $user = User::create([
+$user = User::where('email',$request['email'])->first();
+
+if($user){
+    $response['status'] = 0;
+        $response['message'] = 'Email Already Exists';
+        $response['code'] = 200;
+}else{
+$user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password)
@@ -17,6 +24,9 @@ class UserController extends Controller
         $response['status'] = 1;
         $response['message'] = 'User Registered Successfully';
         $response['code'] = 200;
+}
+
+        
         return response()->json($response);
     }
 }
